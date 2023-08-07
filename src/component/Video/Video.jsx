d94d637982e953d005d57bd1a8e1aba96f7ac399
RNK <rnkitsolutions@gmail.com>
2023-08-07 14:39:09 +0530

    init

---
+++ src/component/Video/Video.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Video.css";
+import ReactPlayer from "react-player";
+
+const Video = () => {
+  return (
+    <section className="s-video">
+      <ReactPlayer
+        url={require("../../assets/v4.mp4")}
+        width="100vw"
+        height="33rem"
+        playing={true}
+        muted={true} // Mute the video to satisfy browser policies
+        loop={true} // Set to 'true' if you want the video to loop
+        controls // Display video controls
+        className="video-player"></ReactPlayer>
+    </section>
+  );
+};
+
+export default Video;
